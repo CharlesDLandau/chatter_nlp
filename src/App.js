b8d46215d3cc8f478b622c37e2eaa022397e9dda
unknown <charlesdlandau@gmail.com>
2018-12-21 15:23:44 -0400

    ui push options

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import './App.css';
 import ChatUI from './components/ChatUI/ChatUI.js'
+
+
+import Typography from '@material-ui/core/Typography';
+
+import red from '@material-ui/core/colors/red';
+import grey from '@material-ui/core/colors/grey';
+
+import Backdrop from '@material-ui/core/Backdrop';
+import NoSsr from '@material-ui/core/NoSsr';
+
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from '@material-ui/core/styles/';
+
+const theme = createMuiTheme({
+  palette: {
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <ChatUI/>
-        </header>
-      </div>
+          <MuiThemeProvider theme={theme}><NoSsr><Backdrop open/><CssBaseline/>
+	<ChatUI/></NoSsr></MuiThemeProvider>
+        
+      
     );
   }
 }
--- src/components/ChatUI/ChatUI.js
+++ src/components/ChatUI/ChatUI.js
@@ -1,14 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component, Fragment } from 'react';
 import SocketIO from 'socket.io-client';
 
 import Card from '@material-ui/core/Card';
 import FormControl from '@material-ui/core/FormControl';
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -32,6 +35,15 @@ const styles = theme => ({
     border: "solid",
     borderColor: "#262626"
   },
+  ownMessage: {
+    justify: "left",
+  },
+  receivedMessage: {
+    justify: "right",
+    align: "right",
+    padding: theme.spacing.unit*2,
+
+  },
   chatForm: {
     margin: theme.spacing.unit,
   },
@@ -45,8 +57,12 @@ class ChatUI extends Component {
     constructor( props ) {
       super( props );
       this.state = {
-        m:"Hello",
-        s:"Hello"
+        m:"",
+        recievedMessages:[
+        {name: "app", message: "Hello"}
+        ],
+        ownMessages: [
+        ]
       };
   }
 
@@ -55,16 +71,22 @@ class ChatUI extends Component {
     this.setState({
       [name]: event.target.value,
     }, ()=>{
-      console.log(this.state[name])
     });
     
   };
+
+  pushUIMessage = name => event => {
+    this.setState({
+      ownMessages: [...this.state.ownMessages, 
+      {name: name, message: this.state.m}]
+    }, ()=>{console.log(this.state)})
+  }
+
   handleSubmit = name => event => {
       
       this.setState({
         [name]: this.state.m,
       }, ()=>{
-        console.log(this.state[name])
       });
       
   };
@@ -74,45 +96,81 @@ class ChatUI extends Component {
     const { classes } = this.props
     return(
       <div className={classes.container}>
+      <Grid item xs>
       <Card className={classes.chatCard}
       >
 
-
-
-      <CardContent className={classes.chatBody}>
-        <span>{this.state.s}</span>
-      </CardContent>
+      <CardContent>
+    <List>
+    <Grid container spacing={16}
+    direction="column"
+    alignItems="flex-end"
+    justify="flex-end">
+    {this.state.recievedMessages.map((item, idx)=>{
+      return <Grid  key={idx.toString()} item xs><Divider/>
+        <ListItem
+      >
+        <Paper className={
+          classes.ownMessage
+        }><Typography variant="body1">
+        {item.message}:{item.name}
+        </Typography>
+        </Paper></ListItem>
+        </Grid>
+        })}
+    </Grid>
+    <Grid container spacing={16}
+    direction="column"
+    alignItems="flex-start"
+    justify="flex-start">
+    {this.state.ownMessages.map((item, idx)=>{
+      console.log(item)
+      return <Grid key={idx.toString()} item xs><Divider/>
+        <ListItem>
+        <Paper className={
+          classes.ownMessage
+        }><Typography variant="body1">
+        {item.message}:{item.name}
+        </Typography></Paper></ListItem>
+        </Grid>
+        })}
+        </Grid>
+    </List>
+    </CardContent>
 
       <Divider variant="middle" color="secondary"/>
       <CardContent>
+      <FormControl onSubmit={this.pushUIMessage("m")}>
       <Grid container spacing={16}
         direction="row"
         alignItems="stretch"
         justify="flex-end">
       
-
           <Grid item>
           <Button value={this.state.m} type="submit"
-          className={classes.formButton} variant="contained"
-          onClick={this.handleSubmit('s')}>Send</Button>
+          className={classes.formButton}
+          variant="contained"
+          onClick={this.pushUIMessage('m')}>
+          Send</Button>
           </Grid>
           <Grid item xs>
           
           <TextField className={classes.formInput}
-          id="standard-name"
-          label="Name"
           fullWidth
           value={this.state.name}
           onChange={this.handleChange('m')}
           margin="normal"
+          onSubmit={this.pushUIMessage('m')}
           />
           </Grid>
-        
-
+      
       </Grid>
+      </FormControl>
+
       </CardContent>
 
       </Card>
+      </Grid>
       </div>
     )
   }
